97645012da22439cae1e2e0d9481425a5eea6829
seongjin96 <w8348@naver.com>
2021-01-28 14:13:40 +0900

    연습문제

---
+++ Level 1/가운데 글자 가져오기.cpp
@@ -0,0 +1,20 @@
+/*
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 
+단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    if(s.length() % 2 == 1){
+        answer = s[s.length()/2];
+    }else{
+        answer = s[s.length()/2 - 1];
+        answer += s[s.length() / 2];
+    }
+    return answer;
+}
