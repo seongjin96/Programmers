4f7f96894dd426afd0b80500295816823a24b5af
seongjin96 <w8348@naver.com>
2021-11-07 10:33:28 +0900

    2021 카카오 채용연계형 인턴십

---
+++ Level 1/숫자 문자열과 영단어.py
@@ -0,0 +1,32 @@
+'''
+이렇게 숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어집니다. 
+s가 의미하는 원래 숫자를 return 하도록 solution 함수를 완성해주세요.
+'''
+
+
+def solution(s):
+    answer = ''
+    words = {
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9',
+    }
+
+    word = ''
+    for i in s:
+        if i.isdigit():
+            answer += i
+            continue
+        word += i
+        if word in words:
+            answer += words[word]
+            word = ''
+
+    return int(answer)
