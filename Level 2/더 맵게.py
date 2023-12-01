9699e8857be6255172858b30c0f30f68c561bc33
seongjin96 <w8348@naver.com>
2023-12-01 11:32:32 +0900

    힙(Heap)

---
+++ Level 2/더 맵게.py
@@ -0,0 +1,27 @@
+'''
+Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과 원하는 스코빌 지수 K가 주어질 때, 
+모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return 하도록 solution 함수를 작성해주세요.
+'''
+
+scoville = [1, 2, 3, 9, 10, 12]	
+K = 7
+
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            return -1
+        
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+
+        heapq.heappush(scoville, first + (second * 2))
+        answer += 1
+    
+    return answer
+
+print(solution(scoville, K))
