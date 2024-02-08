8428dcd9fa410f24fc8d4a60a9ab4ee49bc41e16
seongjin96 <w8348@naver.com>
2024-02-08 15:36:11 +0900

    연습문제

---
+++ Level 1/바탕화면 정리.py
@@ -0,0 +1,23 @@
+'''
+머쓱이의 컴퓨터 바탕화면의 상태를 나타내는 문자열 배열 wallpaper가 매개변수로 주어질 때
+바탕화면의 파일들을 한 번에 삭제하기 위해 최소한의 이동거리를 갖는 드래그의 시작점과 끝점을 담은 정수 배열을 return하는 solution 함수를 작성해 주세요.
+드래그의 시작점이 (lux, luy), 끝점이 (rdx, rdy)라면 정수 배열 [lux, luy, rdx, rdy]를 return하면 됩니다.
+'''
+
+wallpaper = ["..", "#."]
+
+def solution(wallpaper):
+    lux = luy = 50
+    rdx = rdy = 0
+
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[0])):
+            if wallpaper[i][j] == '#':
+                lux = min(lux, i)
+                luy = min(luy, j)
+                rdx = max(rdx, i + 1)
+                rdy = max(rdy, j + 1)
+
+    return [lux, luy, rdx, rdy]
+
+print(solution(wallpaper))
